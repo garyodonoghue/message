7544896f040c1bdd632974d8761accddf6cb07e6
Gary O' Donoghue <gary.odonoghue@gmail.com>
2014-01-26 17:37:25 +0000

    add automcomplete text box for phone contacts

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -27,7 +27,13 @@ namespace DrunkyTexty
         {
             InitializeComponent();
             getContacts();
-            NumberData.Text = "l";
+
+            generateCaptcha();
+        }
+
+        private void generateCaptcha()
+        {
+            throw new NotImplementedException();
         }
 
         private void getContacts()
@@ -42,13 +48,9 @@ namespace DrunkyTexty
         private void ContactsSearchCompleted(object sender, ContactsSearchEventArgs e)
         {
             cons.SearchCompleted -= ContactsSearchCompleted;
-            //e.Results should be the list of contact, since there's no filter applyed in the search you shoul have all contact here
-
-            //System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            //_Number.ItemsSource = e.Results;
-            //NumberData.ItemsSource = e.Results;
+       
             listCons = e.Results;
-            NumberData.ItemsSource = listCons;
+            autoCompleteBox1.ItemsSource = listCons.Cast<object>().ToList();
         }
  
 
@@ -56,9 +58,62 @@ namespace DrunkyTexty
         {
             SmsComposeTask smsComposeTask = new SmsComposeTask();
 
-            smsComposeTask.To = NumberData.Text;
+            smsComposeTask.To = autoCompleteBox1.Text;
             smsComposeTask.Body = _Message.Text;
             smsComposeTask.Show();
+        }
+    }
+
+    public partial class CImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Create a random code and store it in the Session object.
+            this.Session["CaptchaImageText"] = GenerateRandomCode();
+            // Create a CAPTCHA image using the text stored in the Session object.
+            RandomImage ci = new RandomImage(this.Session
+                ["CaptchaImageText"].ToString(), 300, 75);
+            // Change the response headers to output a JPEG image.
+            this.Response.Clear();
+            this.Response.ContentType = "image/jpeg";
+            // Write the image to the response stream in JPEG format.
+            ci.Image.Save(this.Response.OutputStream, ImageFormat.Jpeg);
+            // Dispose of the CAPTCHA image object.
+            ci.Dispose();
+        }
+
+        // Function to generate random string with Random class.
+        private string GenerateRandomCode()
+        {
+            Random r = new Random();
+            string s = "";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = r.Next(3);
+                int ch;
+                switch (i)
+                {
+                    case 1:
+                        ch = r.Next(0, 9);
+                        s = s + ch.ToString();
+                        break;
+                    case 2:
+                        ch = r.Next(65, 90);
+                        s = s + Convert.ToChar(ch).ToString();
+                        break;
+                    case 3:
+                        ch = r.Next(97, 122);
+                        s = s + Convert.ToChar(ch).ToString();
+                        break;
+                    default:
+                        ch = r.Next(97, 122);
+                        s = s + Convert.ToChar(ch).ToString();
+                        break;
+                }
+                r.NextDouble();
+                r.Next(100, 1999);
+            }
+            return s;
         }
     }
 }
